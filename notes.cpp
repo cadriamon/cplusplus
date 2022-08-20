7a41d461fdfde38ee78e0381dea7beedd743380e
Chris Montebon <cadriamon@gmail.com>
2022-08-19 23:44:59 -0400

    08192022, notes update

--- notes.cpp
+++ notes.cpp
@@ -1,6 +1,9 @@
 // C++ is a compiled language
 // git init
+// git remote add origin url
 // git add .
 // git commit -m "Message"
-// git remote add origin url
 // git push -u origin master
+// Create .exe in bash: g++ name.cpp -o exename
+// Execute file ./exename
+// To execute machine code file ./a.out
