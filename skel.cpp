b70c53d2b3b27fcc8d59ae663ebc72726952a8c2
Chris Montebon <cadriamon@gmail.com>
2022-08-23 00:00:03 -0400

    0822022

--- skel.cpp
+++ skel.cpp
@@ -4,4 +4,11 @@ int main()
 {
     // Your program lives here
     std::cout << "Test";
+    std::cout << "Test\n";
+    std::cout << "CCCC\n";
+    std::cout << "C  C\n";
+    std::cout << "C   \n";
+    std::cout << "C   \n";
+    std::cout << "C  C\n";
+    std::cout << "CCCC\n";
 }
