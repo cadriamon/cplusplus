8a5b84a262ea824ae5ce603ed451e2e106c9abee
Chris Montebon <cadriamon@gmail.com>
2022-08-25 23:52:47 -0400

    08252022

--- skel.cpp
+++ skel.cpp
@@ -5,6 +5,7 @@ int main()
     // Variable declaration
     int year;
     int score = 0;
+    int score_1 = 1234 * 99;
 
     // Your program lives here
     std::cout << "Test";
@@ -14,4 +15,5 @@ int main()
     std::cout << "C     M    M    M\n";
     std::cout << "C  C  M         M\n";
     std::cout << "CCCC  M         M\n";
+    std::cout << score_1 << "\n";
 }
